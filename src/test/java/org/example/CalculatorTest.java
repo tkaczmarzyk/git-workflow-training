ee68adbb02c2baaaf772700b2317c54eb0f74cb9
Anet93 <anetyurchak@gamil.com>
2015-07-06 11:03:53 +0200

    poprawka testu

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -4,15 +4,14 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-
 public class CalculatorTest {
-    
-    Calculator calc = new Calculator();
-    
-    @Test
-    public void subtracts2Numbers() {
-        int result = calc.subtract(2, 2);
-        
-        assertEquals(0, result);
-    }
+
+	Calculator calc = new Calculator();
+
+	@Test
+	public void subtracts2Numbers() {
+		int result = calc.subtract(3, 2);
+
+		assertEquals(1, result);
+	}
 }
